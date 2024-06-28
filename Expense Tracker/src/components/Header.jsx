f0456602eb14780c28c0df32d51cff280abbe05f
APSK22 <158535097+APSK22@users.noreply.github.com>
2024-06-28 09:48:06 +0530

    Expense Tracker

---
+++ Expense Tracker/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div classname='header-section'>
+    <h2 className="header">
+      Expense Tracker
+    </h2>
+    <img src='./finance.png' className="money-icon" alt="Finance Icon" />
+    </div>
+
+  )
+}
